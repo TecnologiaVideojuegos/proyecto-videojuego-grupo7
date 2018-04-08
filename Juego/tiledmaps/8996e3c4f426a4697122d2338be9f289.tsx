e10ec55e03c23b8642ad7d368328c6bed0978d7d
AngelOroquieta <angeloroquieta@gmail.com>
2018-04-08 20:53:31 +0200

    Mapa puebloMontana
    Actualizo la lista de mapas con el puebloMontana, el igual que el del puerto, me gustaria que me dijerais que os parece, si quereis que modifique algo antes de meter los NPCs y las zonas de generacion de enemigos. Las colisiones ya estan metidas.

---
+++ Juego/tiledmaps/8996e3c4f426a4697122d2338be9f289.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="8996e3c4f426a4697122d2338be9f289" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="8996e3c4f426a4697122d2338be9f289.png" width="768" height="768"/>
+</tileset>
