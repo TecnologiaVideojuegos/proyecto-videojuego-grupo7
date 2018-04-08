9fd629aa13da1f015bca8eb21a3ab4652d9a9035
AngelOroquieta <angeloroquieta@gmail.com>
2018-04-08 17:14:23 +0200

    Mapa puerto con colisiones
    Primer patron del mapa del puerto con colisiones

---
+++ Juego/tiledmaps/img_1583_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="img_1583_01" tilewidth="32" tileheight="32" tilecount="252" columns="18">
+ <image source="img_1583_01.jpg" width="600" height="450"/>
+</tileset>
