19216db81a32d96c090b0dd3d35f0bfa6884fa10
AngelOroquieta <angeloroquieta@gmail.com>
2018-04-13 16:04:44 +0200

    Actualizacion grafica puerto y puebloMontana
    Estado gráfico final de los mapas puerto y puebloMontana, a falta de eventos.

---
+++ Juego/tiledmaps/e5d1aa18eca1590a495df1ea4479c774.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="e5d1aa18eca1590a495df1ea4479c774" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="e5d1aa18eca1590a495df1ea4479c774.png" width="512" height="512"/>
+</tileset>
