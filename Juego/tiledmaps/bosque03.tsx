d2f44321befe188b9bece0d5063c691bce592cba
Alberto Murillo <alberto.murillo@edu.uah.es>
2018-04-14 10:58:40 +0200

    Creado Mapa Bosque Final
    No está terminado

---
+++ Juego/tiledmaps/bosque03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bosque03" tilewidth="32" tileheight="32" tilecount="594" columns="27">
+ <image source="bosque03.png" width="864" height="705"/>
+</tileset>
