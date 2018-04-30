70fc09f7384075e0575f8830df13b558ef0f87eb
PContreras8 <37660373+PContreras8@users.noreply.github.com>
2018-04-30 17:15:37 +0200

    MapaTutorial a falta de completar
    No me funciona el mapa al añadir enemigos, si alguien me puede ayudar por favor.
    Eventos está completo y bloqueos tambien

---
+++ Juego/tiledmaps/MapaTutorial/SpriteGente.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SpriteGente" tilewidth="32" tileheight="32" tilecount="96" columns="12">
+ <image source="../Imagenes/Animaciones/Sprites/People1.png" width="384" height="256"/>
+</tileset>
